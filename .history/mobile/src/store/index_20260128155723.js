685e6cef489c8826da55b3a60605d2ef9ab8054a
HazeHacker <3542495583@qq.com>
2026-02-04 21:54:54 +0800

    chore: 删除历史文件
    - 删除.history
    - 更新.gitignore

--- .history/mobile/src/store/index_20260128155723.js
+++
@@ -1,5 +0,0 @@
-/**
- * Store统一导出
- */
-export { useUserStore } from './user'
-export { useChatStore } from './chat'
--- .history/mobile/src/store/index_20260204184233.js
+++
@@ -1,9 +0,0 @@
-/**
- * Store统一导出
- */
-export { useUserStore } from './user'
-export { useChatStore } from './chat'
-export { useDashboardStore } from './dashboard'
-export { useGlucoseCurveStore } from './glucoseCurve'
-export { useHealthPlanStore } from './healthPlan'
-export { useCompanionStore } from './companion'
